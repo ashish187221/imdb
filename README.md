6d610ac9e3c3988f46498c0d9f9f91dcad4a0327
Ashish <ashish187.be22@chitkara.edu.in>
2024-04-19 12:06:35 +0530

    First

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# imdb
+# imdb-c
+This is an most likely imdb clone project created with help of React JS along with some taste of Material UI for designing of UI
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+   <div className="footer">
+    Created by : Ashish Rana
+   </div>
+  )
+}
+
+export default Footer
